299786c853b515ce66a7191509adcb5de55bf4b6
kkhindigyan01 <kkhindigyan01@gmail.com>
2021-06-11 20:22:48 +0530

    first commit

---
+++ src/main/java/com/kkhindigyan/app/SpringBootRestfulApiWithOracleDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.kkhindigyan.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestfulApiWithOracleDatabaseApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(SpringBootRestfulApiWithOracleDatabaseApplication.class, args);
+	}
+}
