3fd9a45a10cb06c4179d0d0475957a2432363a61
Sarthak Shyngle <50234097+Sarthak160@users.noreply.github.com>
2023-01-05 13:16:06 +0530

    feat: keploy integrate
    Signed-off-by: Sarthak Shyngle <50234097+Sarthak160@users.noreply.github.com>

--- src/main/java/com/kkhindigyan/app/SpringBootRestfulApiWithOracleDatabaseApplication.java
+++ src/main/java/com/kkhindigyan/app/SpringBootRestfulApiWithOracleDatabaseApplication.java
@@ -1,11 +1,15 @@
 package com.kkhindigyan.app;
 
+import io.keploy.ksql.KDriver;
+import io.keploy.ksql.KResultSet;
+import io.keploy.servlet.KeployMiddleware;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(KeployMiddleware.class)
 public class SpringBootRestfulApiWithOracleDatabaseApplication {
-
 	public static void main(String[] args) {
 		
 		SpringApplication.run(SpringBootRestfulApiWithOracleDatabaseApplication.class, args);
